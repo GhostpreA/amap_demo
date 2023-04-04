5559d6963fe19573aeb2ea93d4acd92cfde5afdb
郭宇轩 <1725985629@qq.com>
2023-04-04 23:35:41 +0800

    Amap init success

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,23 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <map-container></map-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import MapContainer from '@/map/MapContainer.vue'
+
+
 
 export default {
   name: 'HomeView',
   components: {
+    MapContainer,
     HelloWorld
-  }
+  },
+
 }
 </script>
